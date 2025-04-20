ac408ba90ba718ab00ade42eae5cff1f3ea99d47
Sagar <nobby@tellofy.com>
2025-04-20 23:33:03 +0530

    update

--- components/home/HeroBanner/index.jsx
+++ components/home/HeroBanner/index.jsx
@@ -4,17 +4,18 @@ const HeroBanner = () => {
   return (
     <section className='ui-heroBanner' observer-animation="cssClass" observer-animation-classes="animateImagesIn" observer-animation-repeat="true">
       <div className="ui-heroBanner__wrapper">
-        <AutoplayVideo className="ui-heroBanner__video"
+        <AutoplayVideo 
+          wrapperClass="ui-heroBanner__video-wrapper" 
           autoPlay
           muted
           loop
           playsInline
           preload="auto"
-          fetchPriority="high"  
+          fetchPriority="high"
           desktopSrc={"https://ik.imagekit.io/bn4qs1mj3/DesktopHeroVideoHQ.mp4?updatedAt=1744977909415"}
           mobileSrc={"/videos/MobileHeroVideo.mp4"}
-          /> 
-        <div className='ui-heroBanner__content'> 
+        />
+        <div className='ui-heroBanner__content'>
           <h1 className='ui-heroBanner__heading' observer-animation="title">
             Magnetic Maharashtra
             is your hub for
